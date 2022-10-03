8b388b767b073d8881dc46faaf9494f365d4a624
Scotch <blackscotch@mail.ru>
2022-10-03 06:40:29 +0300

    программа написана полностью

--- hw/Program.cs
+++ hw/Program.cs
@@ -3,21 +3,48 @@
 Console.WriteLine("Привет, человек! Тебе предстоит выполнить тестирование этого маленького кусочка кода. Спасибо за труды :)");
 Console.WriteLine("Ты можешь задать с клавиатуры строковый массив, его элементы разделяй запятыми. Или нажми Enter для работы с заданным программой массивом.");
 
+string input = String.Empty;
+
 string[] myArr = new string[] {"Hello", "2", "world", ":-)"};
 
-Console.Write("Введи массив через запятую (пробел - тоже символ), или жми Enter: ");
-string input = Console.ReadLine();
+Console.Write("Введи массив (пробел - тоже символ), или жми Enter: ");
+input = Console.ReadLine();
 
 if (input == "" || input == null)
 {
+    Console.WriteLine();
     Console.WriteLine("Был нажат Enter или строка пуста, потому работаем с заданным массивом");
-    Console.WriteLine($"{String.Join(", ", GetNewArray(myArr))}");
+    Console.WriteLine("Вот массив для работы:");
+    PrintArray(myArr);
+    Console.WriteLine();
+
+    if(GetNewArray(myArr) != null)
+    {
+        Console.WriteLine($"Новый массив с элементами < 4-х символов: [{String.Join(", ", GetNewArray(myArr))}];");
+    }
+    else
+    {
+        Console.WriteLine($"В массиве [{String.Join(", ", myArr)}] нет подходящих элементов");
+    }
 }
 else
 {
     char[] separators = new char[] {','};
     myArr = input.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-    Console.WriteLine($"{String.Join(", ", GetNewArray(myArr))}");
+    Console.WriteLine();
+    Console.WriteLine("Был введен массив с клавиатуры, потому работаем  с твоим массивом.");
+    Console.WriteLine($"Вот такой массив ты создал:");
+    PrintArray(myArr);
+    Console.WriteLine();
+    if(GetNewArray(myArr) != null)
+    {
+        Console.WriteLine($"Новый массив с элементами < 4-х символов: [{String.Join(", ", GetNewArray(myArr))}];");
+    }
+    else
+    {
+        Console.WriteLine($"В массиве [{String.Join(", ", myArr)}] нет подходящих элементов");
+    }
+    
 }
 
 
@@ -33,19 +60,32 @@ string[] GetNewArray(string[] array)
             count++;
         }
     }
-    string[] newArray = new string[count];
 
-    count = 0;
+    if (count > 0)
+    {
+        string[] newArray = new string[count];
 
-    for (int i = 0; i < array.Length; i++)
-    {   
-        if(array[i].Length <= 3)
-        {
-            newArray[count] = array[i];
-            count++;
+        count = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {   
+            if(array[i].Length <= 3)
+            {
+                newArray[count] = array[i];
+                count++;
+            }
         }
-    }
 
-    return newArray;
+        return newArray;
+    }
+    else return null;
+    
 }
 
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{array[i]},");
+    }
+}
