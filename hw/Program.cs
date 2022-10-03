8bfc07d78699eab6a7707a41ddff9cb26f346395
Scotch <blackscotch@mail.ru>
2022-10-03 04:01:34 +0300

    основная логика работы програмы

--- hw/Program.cs
+++ hw/Program.cs
@@ -1,17 +1,26 @@
-﻿Console.WriteLine("Привет, человек! Тебе предстоит выполнить тестирование этого маленького кусочка кода. Спасибо за труды :)");
+﻿Console.Clear();
 
+Console.WriteLine("Привет, человек! Тебе предстоит выполнить тестирование этого маленького кусочка кода. Спасибо за труды :)");
 Console.WriteLine("Ты можешь задать с клавиатуры строковый массив, его элементы разделяй запятыми. Или нажми Enter для работы с заданным программой массивом.");
-
-// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
-// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
-// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
-// лучше обойтись исключительно массивами.
-// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 
 string[] myArr = new string[] {"Hello", "2", "world", ":-)"};
 
-// Console.ReadLine();
-Console.WriteLine($"{String.Join(", ", GetNewArray(myArr))}");
+Console.Write("Введи массив через запятую (пробел - тоже символ), или жми Enter: ");
+string input = Console.ReadLine();
+
+if (input == "" || input == null)
+{
+    Console.WriteLine("Был нажат Enter или строка пуста, потому работаем с заданным массивом");
+    Console.WriteLine($"{String.Join(", ", GetNewArray(myArr))}");
+}
+else
+{
+    char[] separators = new char[] {','};
+    myArr = input.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    Console.WriteLine($"{String.Join(", ", GetNewArray(myArr))}");
+}
+
+
 
 string[] GetNewArray(string[] array)
 {
@@ -39,3 +48,4 @@ string[] GetNewArray(string[] array)
 
     return newArray;
 }
+
